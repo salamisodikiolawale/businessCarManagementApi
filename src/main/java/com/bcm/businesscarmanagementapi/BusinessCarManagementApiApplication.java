af2bfc7803b5539af69d8eecd8a364e33eb2ec71
salamisodikiolawale <salamisodikiolawale@gmail.com>
2023-10-07 10:30:46 +0200

    first commit

---
+++ src/main/java/com/bcm/businesscarmanagementapi/BusinessCarManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.bcm.businesscarmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessCarManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessCarManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bcm/businesscarmanagementapi/BusinessCarManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcm.businesscarmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessCarManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
