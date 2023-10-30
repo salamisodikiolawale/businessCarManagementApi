ae2cadcef2a550c17cb5341c902d05b8c215d37b
salamisodikiolawale <salamisodikiolawale@gmail.com>
2023-10-30 20:34:36 +0100

    authority

--- src/main/java/com/bcm/businesscarmanagementapi/entity/Users.java
+++ src/main/java/com/bcm/businesscarmanagementapi/entity/Users.java
@@ -1,7 +1,13 @@
 package com.bcm.businesscarmanagementapi.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Builder
@@ -12,12 +18,17 @@ import lombok.*;
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
+    @Column(name = "user_id", nullable = false)
     private Long id;
     @Column(name = "email", nullable = false, unique = true)
     private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(name = "password", nullable = false)
     private String password;
     @Column(name = "role", nullable = false)
     private String role;
+    @JsonIgnore //Pour éviter une boucle infini et ignorer le champs dans le retour json au front (donnée sensible)
+//    @JsonManagedReference //Pour éviter une boucle infini et afficher le champs  dans le retour json au front (donnée sensible) , il faut ajouter //    @JsonBackReference dans Entity User
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+    private Set<Authority> authorities;
 }
